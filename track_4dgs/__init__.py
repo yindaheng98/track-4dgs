b24e578ace462cbe8c6d1e9afcb7b50fae256d39
yindaheng98 <yindaheng98@gmail.com>
2026-05-07 16:51:00 -0700

    export

--- track_4dgs/__init__.py
+++ track_4dgs/__init__.py
@@ -1,4 +1,5 @@
 from .cotracker import Cotracker3PointTracker
+from .vggt import VGGTPointTracker
 from .registry import register_point_tracker, get_available_point_trackers, build_point_tracker
 from .tracker import Query, Track, AbstractPointTracker
 from .tracker import CameraTrack, TrackedCameraDataset, CameraDatasetTracker
@@ -7,5 +8,5 @@ __all__ = [
     "Query", "Track", "AbstractPointTracker",
     "CameraTrack", "TrackedCameraDataset", "CameraDatasetTracker",
     "register_point_tracker", "get_available_point_trackers", "build_point_tracker",
-    "Cotracker3PointTracker",
+    "Cotracker3PointTracker", "VGGTPointTracker",
 ]
