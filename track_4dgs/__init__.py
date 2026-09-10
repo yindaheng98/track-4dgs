e78e45dd48430a3bbc4555c9d7b005d611effda3
yindaheng98 <yindaheng98@gmail.com>
2026-09-09 23:16:25 -0700

    export AbstractBatchPointTracker

--- track_4dgs/__init__.py
+++ track_4dgs/__init__.py
@@ -3,12 +3,12 @@ from .vggt import VGGTPointTracker
 from .mvtap import MVTAPPointTracker
 from .registry import register_point_tracker, get_available_point_trackers, build_point_tracker
 from .tracker import Query, Track, AbstractPointTracker
-from .tracker import AbstractViewPointTracker
+from .tracker import AbstractViewPointTracker, AbstractBatchPointTracker
 from .tracker import CameraTrack, TrackedCameraDataset, CameraDatasetTracker
 
 __all__ = [
     "Query", "Track", "AbstractPointTracker",
-    "AbstractViewPointTracker",
+    "AbstractViewPointTracker", "AbstractBatchPointTracker",
     "CameraTrack", "TrackedCameraDataset", "CameraDatasetTracker",
     "register_point_tracker", "get_available_point_trackers", "build_point_tracker",
     "Cotracker3PointTracker", "VGGTPointTracker", "MVTAPPointTracker",
